c697d102f0a4c63d763d9bbe250060814c3895e8
safadtm <safadtm2004@gmail.com>
2024-03-24 16:56:36 +0530

    Created pg14.cpp

---
+++ pg14.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Integer{
+    int m,n;
+
+    public:
+    Integer(){
+        m=0;
+        n=0;
+    }
+    Integer(int a,int b){
+   m=a;
+   n=b;
+    }
+    Integer(Integer &i){
+       m=i.m;
+       m=i.n;
+    }
+    void display(){
+        cout<<"m="<<m<<",n="<<n<<endl;
+    }
+};
+
+int main(){
+  Integer I1;
+  Integer I2(20,40);
+  Integer I3(I2);
+I1.display();
+  I2.display();
+  I3.display();
+    return 0;
+}
