eb1a45996979ff25e189c7f079ec73f15cecc3ab
Safad T M <106598178+safadtm@users.noreply.github.com>
2024-07-11 14:22:04 +0530

    Create pg22.cpp

---
+++ pg22.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespce std;
+
+class Space{
+int x,y,z;
+public:void getdata(int a,int b,int c);
+void display();
+void operator-();
+};
+
+void Space::getdata(int a,int b,int c){
+    x=a;
+    y=b;
+    z=c;
+}
+
+void Space::display(){
+    cout<<"x="<<x<<endl<<"Y="<<y<<endl<<"Z="<<z;    
+}
+
+void Space::operator-(){
+    x=x;
+    y=y;
+    z=z;
+}
+
+int main(){
+    Space s;
+    s.getdata(10,20,30);
+    s.display();
+    -s;
+    cout<<endl<<"After Overloading";
+    s.display();
+    return 0;
+}
